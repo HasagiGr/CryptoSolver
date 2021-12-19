a017de5b72f0beaf9ec8bc83e5d329fa619c8580
Артем Грязнов <181397@edu.fa.ru>
2021-12-19 17:34:00 +0300

    abc

--- CryptoSolver/Bot.cs
+++ CryptoSolver/Bot.cs
@@ -59,122 +59,58 @@ namespace CryptoSolver
                     {
                         await client.SendTextMessageAsync(
                                 chatId: msg.Chat.Id,
-                                "Вы ввели неверные значений!",
+                                "Вы ввели неверные значения!",
                                 replyMarkup: GeneralMenu());
+                        Path = new List<string>();
+                    }
+                    catch (KeyNotFoundException)
+                    {
+                        await client.SendTextMessageAsync(
+                                chatId: msg.Chat.Id,
+                                "Продолжишь тыкать и бот умрет!",
+                                replyMarkup: GeneralMenu());
+                        Path = new List<string>();
+                    }
+                    catch
+                    {
+                        await client.SendTextMessageAsync(
+                                chatId: msg.Chat.Id,
+                                "Тебе меня не сломать!",
+                                replyMarkup: GeneralMenu());
+                        Path = new List<string>();
                     }
                 }
                 // Первый уровень
                 if (Path.Count == 1)
                 {
-                    isAnyOtherMessage = false;
-                    switch (Path[0])
+                    try
                     {
-                        case "Шифры":
-                            Path.Add(msg.Text);
-                            switch (msg.Text)
-                            {
-                                case "RSA":
-                                    await client.SendTextMessageAsync(
-                                        chatId: msg.Chat.Id,
-                                        "Введите данные в фомате x,n,e");
-                                    break;
-                                case "El Gamal":
-                                    await client.SendTextMessageAsync(
-                                        chatId: msg.Chat.Id,
-                                        "Введите данные в фомате x,n,q,r");
-                                    break;
-                            }
-                            break;
-                        case "Калькулятор":
-                            Path.Add(msg.Text);
-                            switch (msg.Text)
-                            {
-                                case "Разложение на множители":
-                                    await client.SendTextMessageAsync(
-                                        chatId: msg.Chat.Id,
-                                        "Введите x");
-                                    break;
-                                case "Быстрое экспоненцирование":
-                                    await client.SendTextMessageAsync(
-                                        chatId: msg.Chat.Id,
-                                        "Введите x, mod, количество шагов");
-                                    break;
-                                case "y = a^b mod p":
-                                    await client.SendTextMessageAsync(
-                                       chatId: msg.Chat.Id,
-                                       "Введите a,b,p");
-                                    break;
-                                case "y = a*b mod p":
-                                    await client.SendTextMessageAsync(
-                                       chatId: msg.Chat.Id,
-                                       "Введите a,b,p");
-                                    break;
-                                case "Минимальная циклическая группа":
-                                    await client.SendTextMessageAsync(
-                                       chatId: msg.Chat.Id,
-                                       "Введите поле field, если необходима циклическая группа больше чем p, введите число p");
-                                    break;
-                                case "Метод тривиальных(простых) делений":
-                                    await client.SendTextMessageAsync(
-                                       chatId: msg.Chat.Id,
-                                       "Введите число для проверки");
-                                    break;
-                                case "Метод Ферма":
-                                    await client.SendTextMessageAsync(
-                                       chatId: msg.Chat.Id,
-                                       "Введите число");
-                                    break;
-                                case "Тест Ферма":
-                                    await client.SendTextMessageAsync(
-                                       chatId: msg.Chat.Id,
-                                       "Введите число x,свидетеля( по умолчанию стоит 2)");
-                                    break;
-                                case "Обратный элемент":
-                                    await client.SendTextMessageAsync(
-                                       chatId: msg.Chat.Id,
-                                       "Введите данные в формате x,mod");
-                                    break;
-                            }
-                            break;
-                        case "Свойства":
-                            Path.Add(msg.Text);
-                            switch (msg.Text)
-                            {
-                                case "Свойства RSA":
-                                    await client.SendPhotoAsync(
-                                        chatId: msg.Chat.Id,
-                                        photo: "https://sun9-52.userapi.com/impg/77trFRESV5SsqXglyWd19QbNtCU1og0x7-TD_A/wczzEj7N5mE.jpg?size=755x1075&quality=96&sign=3cfb442d84f79146d3f29104cb77a6cc&type=album",
-                                         replyMarkup: GeneralMenu());
-                                    break;
-                                case "Свойства El Gamal":
-                                    await client.SendPhotoAsync(
-                                        chatId: msg.Chat.Id,
-                                        photo: "https://sun9-8.userapi.com/impg/ie5AxO8oi-ZygXgj2mZw-qQPjj6bJCV7kyhrYw/H5RgztICWNY.jpg?size=743x626&quality=96&sign=1204a3f13c8bb2485f99143c3cd8c411&type=album",
-                                         replyMarkup: GeneralMenu());
-                                    break;
-                                case "Свойства сравнений":
-                                    await client.SendPhotoAsync(
-                                        chatId: msg.Chat.Id,
-                                        photo: "https://sun9-67.userapi.com/impg/0090ne0xZ709xwNjtCaGnAg4hu3PTXhXe9hQig/x69TBhFDzB0.jpg?size=738x517&quality=96&sign=9c7acc8e9b6316c6fac0f7177e67f023&type=album",
-                                         replyMarkup: GeneralMenu());
-                                    break;
-                                case "Малая теорема Ферма":
-                                    await client.SendPhotoAsync(
-                                        chatId: msg.Chat.Id,
-                                        photo: "https://sun9-62.userapi.com/impg/qfc3-r8Xea1cZfK06gmTEAVnMG6LxsWO1c3x8w/v5LAUDHfwE8.jpg?size=715x703&quality=96&sign=53a02996b0f3bd32ada18872d7e5b5d5&type=album",
-                                         replyMarkup: GeneralMenu());
-                                    break;
-                                case "Теорема Эйлера":
-                                    await client.SendPhotoAsync(
-                                        chatId: msg.Chat.Id,
-                                        photo: "https://sun9-1.userapi.com/impg/dY7ojGRmYDWh3eCccY8Zg95jyM0hW3s6F-_kdA/g809y17OXhU.jpg?size=744x488&quality=96&sign=e35e8f1d806b24c619c992f80b8ea7b7&type=album",
-                                         replyMarkup: GeneralMenu());
-                                    break;
-
-
-                            }
+                        isAnyOtherMessage = false;
+                        Path.Add(msg.Text);
+                        var reply = new Second_Level(msg.Text);
+                        if (reply.Key.Contains('.'))
+                        {
+                            await client.SendTextMessageAsync(
+                                            chatId: msg.Chat.Id,
+                                            reply.Key,
+                                            replyMarkup: GeneralMenu());
                             Path = new List<string>();
-                            break;
+                        }
+                        else
+                        {
+                            await client.SendTextMessageAsync(
+                                            chatId: msg.Chat.Id,
+                                            reply.Key);
+                        }
+                    }
+                    catch (KeyNotFoundException)
+                    {
+                        await client.SendTextMessageAsync(
+                                chatId: msg.Chat.Id,
+                                "Дебил, с кнопочек выбери!",
+                                replyMarkup: GeneralMenu());
+                        Path = new List<string>();
+
                     }
                 }
 
@@ -229,7 +165,7 @@ namespace CryptoSolver
 
             }
         }
-        
+
         private static IReplyMarkup GeneralPropertiesMenu()
         {
             return new ReplyKeyboardMarkup()
